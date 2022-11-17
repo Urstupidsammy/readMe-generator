715a59f2afa375f1ff1daa2e150427526ae46e26
Urstupidsammy <sgalban4@gmail.com>
2022-11-17 01:34:28 -0800

    add function for license badge and function for license link, issue in index.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,16 +3,37 @@ const inquirer = require('inquirer');
 const index = require('../index');
 
 
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Returns a license badge based on what license is given
+// If there is no license, it will return an empty string
 function renderLicenseBadge(license) {
   let = '';
-  if(license === MIT)
-}
+  if(license === MIT){
+    badge = '![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)'
+  } else if (license === 'Apache 2.0') {
+    badge = '![license](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
+  }  else if (license === 'GPL v3.0') {
+    badge = '![license](https://img.shields.io/badge/License-GPLv3-blue.svg)'
+    
+    } else {
+      badge = ""
+    }
+    return badge;
+  }
 
-// TODO: Create a function that returns the license link
+// 
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = '';
+  if(license === MIT) {
+    licenseLink = 'https://choosealicense.com/licenses/mit/'
+  } else if (license === 'Apache 2.0') {
+    licenseLink = 'http://www.apache.org/licenses/LICENSE-2.0'
+  } else if (license === 'GPL v3.0'){
+  licenseLink = 'https://www.gnu.org/licenses'
+  } else {
+    licenseLink = ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
